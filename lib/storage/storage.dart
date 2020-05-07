88308f7d4af59a5f191780ca78629930f3248ac0
chlee1252 <chlee1252@gmail.com>
2020-05-06 23:34:17 -0700

    Added clear method / getItem null catch included

--- lib/storage/storage.dart
+++ lib/storage/storage.dart
@@ -4,11 +4,18 @@ import 'package:sharesns/module/userData.dart';
 class Storage {
   LocalStorage storage = LocalStorage('instaShare.json');
 
+  // Check if the storage is ready
   isReady() async {
     bool ready = await storage.ready;
     return ready;
   }
 
+  // Clearing Item (Dubug Code) Maybe use it later
+  clearItem() async {
+    await storage.clear();
+  }
+
+  // Convert original Data to JSON friendly
   toJSON(Map item) {
     Map hash = Map();
     item.forEach((k, v) {
@@ -17,13 +24,15 @@ class Storage {
     return hash;
   }
 
+  // Set items to local Storage. ID: InstaShare_item
   setItem(item) {
     var newItem = toJSON(item);
     storage.setItem('InstaShare_item', newItem);
   }
 
+  // Get items from the local Storage, if there is none, it returns empty
   getItem() {
-    final item = storage.getItem('InstaShare_item');
+    var item = storage.getItem('InstaShare_item');
     if (item != null) {
       item.forEach((k, v) {
         item[k] = UserData(
@@ -33,6 +42,8 @@ class Storage {
           deepLink: v['deepLink'],
         );
       });
+    } else {
+      item = {};
     }
     return item;
   }
