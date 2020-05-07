3074256d0da76840110b7a3d95f1845afef37dff
chlee1252 <chlee1252@gmail.com>
2020-05-06 22:58:33 -0700

    Error Text design added

--- lib/widget/customInput.dart
+++ lib/widget/customInput.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 class CustomInput extends StatelessWidget {
-  CustomInput({this.controller, this.hintText, this.borderColor, this.obscureText=false, this.onChanged});
+  CustomInput({this.controller, this.hintText, this.borderColor, this.obscureText=false, this.onChanged, this.isError=false});
 
   final TextEditingController controller;
   final String hintText;
   final Color borderColor;
   final bool obscureText;
   final ValueChanged<String> onChanged;
+  final bool isError;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
         width: MediaQuery.of(context).size.width * 0.9,
-        height: 45.0,
         child: TextField(
           controller: this.controller,
           obscureText: this.obscureText,
           onChanged: this.onChanged,
           decoration: InputDecoration(
               hintText: this.hintText,
+              errorText: this.isError ? "Please check your email": null,
               contentPadding: EdgeInsets.all(15.0),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(32.0)),
