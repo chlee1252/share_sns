3bbbd3eb36e2bf9dee1c5f587ef58b5d2ae7f991
chlee1252 <chlee1252@gmail.com>
2020-06-07 23:35:04 -0700

    Provider Implemented on AddScreen

--- lib/screen/addScreen.dart
+++ lib/screen/addScreen.dart
@@ -1,28 +1,20 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sharesns/module/emailValidationProvider.dart';
 import 'package:sharesns/module/userData.dart';
 import 'package:sharesns/sns/snsData.dart';
 import 'package:sharesns/widget/addScreenWidget.dart';
 
 //TODO: Add Screen design and implement
-class AddScreen extends StatefulWidget {
+// ignore: must_be_immutable
+class AddScreen extends StatelessWidget {
   AddScreen({this.title});
-
+  
   static final String id = "AddScreen";
   final String title;
-  @override
-  _AddScreenState createState() => _AddScreenState();
-}
 
-class _AddScreenState extends State<AddScreen> {
   TextEditingController _controller = TextEditingController();
-  bool isError = false;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +24,7 @@ class _AddScreenState extends State<AddScreen> {
         children: [
           Center(
             child: Text(
-              widget.title,
+              this.title,
               style: TextStyle(
                 fontSize: 30.0,
                 fontWeight: FontWeight.bold,
@@ -44,26 +36,25 @@ class _AddScreenState extends State<AddScreen> {
           ),
           CustomInput(
             controller: _controller,
-            hintText: "Please type your ${widget.title} account",
+            hintText: "Please type your ${this.title} account",
             borderColor: Colors.black,
-            isError: this.isError,
+            isError: Provider.of<EmailValidateProvider>(context).isValidate(),
           ),
           CardButton(
               title: "Add",
               onPressed: () {
-                if (widget.title == "Email" && !EmailValidator.validate(_controller.text)) {
-                    setState(() {
-                      this.isError = true;
-                    });
+                if (this.title == "Email" && !EmailValidator.validate(_controller.text)) {
+                  Provider.of<EmailValidateProvider>(context, listen:false).changeValidate(true);
                 } else {
                   if (_controller.text.length != 0) {
-                    var sns = Data.SNSData[widget.title];
+                    var sns = Data.SNSData[this.title];
                     var userData = UserData(
                       imageSrc: sns['image'],
-                      title: widget.title,
+                      title: this.title,
                       deepLink: sns['deeplink'],
                       account: _controller.text,
                     );
+                    Provider.of<EmailValidateProvider>(context, listen:false).cancel(false);
                     Navigator.pop(context, userData);
                   }
                 }
@@ -72,6 +63,7 @@ class _AddScreenState extends State<AddScreen> {
           CardButton(
             title: "Cancel",
             onPressed: () {
+              Provider.of<EmailValidateProvider>(context, listen:false).cancel(false);
               Navigator.pop(context, null);
             },
             color: Colors.grey,
